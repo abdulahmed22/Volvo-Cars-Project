316b29c7e09db06cd59174b59962da20d9c3abd5
OmarAlabdAlla <112431778+OmarAlabdAlla@users.noreply.github.com>
2023-11-20 01:14:41 +0100

    function
    function for visual Analysis of Recycled and Not Recycled Weights Over Time, for both Lager and the rest

--- testProj.py
+++ testProj.py
@@ -1,6 +1,72 @@
 import pandas as pd
 import numpy as np 
+import  matplotlib.pyplot as plt
 
+
+def process_data_and_plot(data, title_suffix=''):
+    # Extract 'Lev.YearMonth' as strings
+    months = data['Lev.YearMonth'].astype(str)
+
+    # Get unique months in the dataset
+    unique_months = months.unique()
+
+    # Initialize lists to store data for each month
+    recycled_sum_per_month = []
+    not_recycled_sum_per_month = []
+
+    # Iterate over each unique month
+    for month in unique_months:
+        # Filter the data for the current month
+        indexes_of_month = months[months.str.contains(month)].index.to_list()
+
+        # Extract 'RodKodbeskr' for the current month
+        data_month = data.loc[indexes_of_month, 'RodKodbeskr']
+
+        # Get indices where 'RodKodbeskr' contains 'Recycle' for the current month
+        indexes_of_Recycled_month = data_month[data_month.str.contains(r'Recycle', case=False)].index.tolist()
+
+        # Get indices where 'RodKodbeskr' does not contain 'Recycle' for the current month
+        indexes_of_Not_Recycled_month = data_month[~data_month.str.contains(r'Recycle', case=False)].index.tolist()
+
+        # Extract weights for recycled and not recycled items for the current month
+        weight_recycled_month = data.loc[indexes_of_Recycled_month, 'Kvantitet kg']
+        weight_Not_recycled_month = data.loc[indexes_of_Not_Recycled_month, 'Kvantitet kg']
+
+        # Sum weights for recycled and not recycled items for the current month
+        recycled_sum_per_month.append(sum(weight_recycled_month))
+        not_recycled_sum_per_month.append(sum(weight_Not_recycled_month))
+
+    # Plotting for Each Month (Total Weight)
+    total_weight_per_month = [total_recycled + total_not_recycled for total_recycled, total_not_recycled in zip(recycled_sum_per_month, not_recycled_sum_per_month)]
+
+    plt.figure(figsize=(10, 6))
+    plt.bar(unique_months, total_weight_per_month, color='blue')
+    plt.xlabel('Month')
+    plt.ylabel(f'Total Weight {title_suffix}')
+    plt.title(f'Total Weight for Each Month in the Year {title_suffix}')
+    plt.xticks(rotation=45, ha='right')
+    plt.show()
+
+    # Plotting for the Whole Year (Together)
+    total_recycled_sum = sum(recycled_sum_per_month)
+    total_not_recycled_sum = sum(not_recycled_sum_per_month)
+
+    # Plotting for Each Month
+    width = 0.35  # the width of the bars
+    months_range = range(len(unique_months))
+
+    plt.bar(months_range, recycled_sum_per_month, width, label='Recycled', color='green')
+    plt.bar(months_range, not_recycled_sum_per_month, width, bottom=recycled_sum_per_month, label='Not Recycled', color='gray')
+
+    # Configure the plot
+    plt.xlabel('Month')
+    plt.ylabel(f'Total Weight {title_suffix}')
+    plt.title(f'Total Weight for Each Month {title_suffix}')
+    plt.xticks(months_range, unique_months)
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
 
 def main():
 
@@ -11,19 +77,19 @@ def main():
     RodKod = df['RodKodbeskr']
     indexes1 = RodKod[RodKod == "-1 N/A"].index.tolist()
     df_filtered = df.drop(index=indexes1)
-    #print(df_filtered)
+    
 
     #filtiring the returants 
     place = df_filtered['Hämtställe Märkning']
     indexes2 = place[place.isin(["Rest. Jacob, kundnr: 0468801", "Rest. Emma, kundnr: 0185512", "Rest. Hive Food Market kundnr: 0468777", "Rest. Höjden, kundnr: 0468785", "Rest. The Harvest (Volvo PVD)"])].index.tolist()
     df_filtered2 = df_filtered.drop(index=indexes2)
-    #print(df_filtered2)
+    
 
     #filtiring the city
     city = df_filtered2['Hämtställe Ort']
     indexes3 = city[city == 'Mölndal'].index.tolist()
     df_filtered3 = df_filtered2.drop(index = indexes3)
-    #print(df_filtered3)
+   
 
 
     #RA
@@ -55,11 +121,51 @@ def main():
 
     data_for_the_lager = df_filtered3.loc[indexes_for_lager]
     data_for_the_rest= df_filtered3.drop(index=indexes_for_lager)
-    weight_amin=data_for_the_lager['Kvantitet kg']
-    print(2222222222222222222222222222222222222222222222222)
+    
 
-    z=sum(weight_amin)
-    print(z)
+    process_data_and_plot(df_filtered3.loc[indexes_for_lager], 'Lager')
+    process_data_and_plot(df_filtered3.drop(index=indexes_for_lager), 'Rest')
+
+###
+
+
+   
+
+
+
+    # Plotting
+    #weight_lager_jan_num=sum(weight_lager_jan)
+    #weight_lager_feb_num=sum(weight_lager_feb)
+    #plt.bar(['January', 'February'], [weight_lager_jan_num, weight_lager_feb_num])
+   
+
+        
+
+    
+    #print(indexes_of_jan)
+    #print(len(indexes_of_jan))
+
+
+
+
+
+
+    #the total for lager
+    #tot_for_lager=data_for_the_lager['Kvantitet kg']
+    #tot_for_lager_x=sum(tot_for_lager)
+    #print(9090909090)
+    #print("The total for lager "+str(tot_for_lager_x))
+
+
+    #for lager in 01
+
+    
+
+
+    #tot_for_Not_lager=data_for_the_rest['Kvantitet kg']
+    #tot_for_lager_y=sum(tot_for_Not_lager)
+    #print(9393939393)
+   # print("The total for Not lager "+str(tot_for_lager_y))
 
 
 
@@ -112,31 +218,16 @@ def main():
     y1=sum(weight_not_rec_not_kager)
     #print(y1)
     tot1=x1+y1
-    #print("total"+tot1)
+
+    print(tot1)
+   
 
 
     
-    print(len(data_not_recycled_not_lager))
-    print(len(data_for_the_rest))
-    print(len(data_for_the_lager))
-    print(len(indexes1)+len(indexes2)+len(indexes3))
+   
 
-    print(444444444445555555555)
-    print(x+y)
-
+   
     
-
-
-
-
-
-
-
-    
-
-
-    #print(rb_data['Kvantitet kg'])
-
 
 
 if __name__ == '__main__':
